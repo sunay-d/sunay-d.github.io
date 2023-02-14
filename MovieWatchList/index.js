3b8e124189502d9a496ee1225eaa5b400d7cf9f1
Sunay <84284117+sunay-d@users.noreply.github.com>
2023-02-15 00:42:28 +0300

    Fixed bugs
    Removed undefined search results, removed undefined properties, fixed localStorage refresh error

--- MovieWatchList/index.js
+++ MovieWatchList/index.js
@@ -1,28 +1,38 @@
 const search = document.getElementById("search-input")
-let watchlist = {movies: {}}
+let watchlist = localStorage.getItem("watchlist")
+let watchlistParsed = JSON.parse(watchlist)
 
 document.addEventListener("click", function(e) {
     if (e.target.id === "search-btn") {
-        render()
+        if(search.value) {
+            render()
+        }else {
+            firstQuery()
+        }
+        search.value = ''
     } else if (e.target.dataset.add) {
         const movie = JSON.parse(e.target.dataset.add)
-        if (watchlist.movies[movie.Title]) {
+        if(!watchlist) {
+            watchlistParsed = {}
+        }
+
+        if (watchlistParsed[movie.Title] != null) {
             removeFromList(movie.Title)
         } else {
             addToList(movie)
         }
-        localStorage.setItem('watchlist',JSON.stringify(watchlist))
+        localStorage.setItem('watchlist',JSON.stringify(watchlistParsed))
     }
 })
 
 function removeFromList(movieTitle) {
     document.getElementById(`ar-${movieTitle}`).textContent = "Watchlist"
     document.getElementById(`arlogo-${movieTitle}`).src = "./img/addicon.png"
-    delete watchlist.movies[movieTitle]
+    delete watchlistParsed[movieTitle]
 }
 
 function addToList(movie) {
-    watchlist.movies[movie.Title] = movie
+    watchlistParsed[movie.Title] = movie
     document.getElementById(`ar-${movie.Title}`).textContent = "Remove"
     document.getElementById(`arlogo-${movie.Title}`).src = "./img/removeicon.png"
 }
@@ -47,42 +57,52 @@ function render() {
 }
 
 function getMovieCard(movie) {
-    let movieJson = JSON.stringify(movie).replace(/'/g, "&#039;")
-    return (`
-    <div class="movie-card">
-    <img class="poster no-margin" src="${movie.Poster}" alt="${movie.Title} poster">
-        <div class="movie-info no-margin">
-            <div class="title no-margin">
-                <h3 class="no-margin">${movie.Title}</h3>
-                <div class="rating no-margin">
-                    <i class="fa fa-star no-margin"></i>
-                    <p class="no-margin">${movie.imdbRating}</p>
+    if (movie) {
+        let movieJson = JSON.stringify(movie).replace(/'/g, "&#039;")
+        function checkNA(property) {
+            return property === "N/A" ? "" : property
+        }
+        return (`
+        <div class="movie-card">
+        <img class="poster no-margin" src="${movie.Poster === "N/A" ? './img/alt-poster.jpg' : movie.Poster}" alt="${movie.Title} poster">
+            <div class="movie-info no-margin">
+                <div class="title no-margin">
+                    <h3 class="no-margin">${checkNA(movie.Title)}</h3>
+                    <div class="rating no-margin">
+                        <i class="fa fa-star no-margin"></i>
+                        <p class="no-margin">${checkNA(movie.imdbRating)}</p>
+                    </div>
                 </div>
-            </div>
-            <div class="details">
-                <div class="time-genre">
-                    <p class="time">${movie.Runtime }</p> 
-                    <p class="genre">${movie.Genre}</p>
+                <div class="details">
+                    <div class="time-genre">
+                        <p class="time">${checkNA(movie.Runtime)}</p> 
+                        <p class="genre">${checkNA(movie.Genre)}</p>
+                    </div>
+                    <div class="add-to-list">
+                        <img class="no-margin" id="arlogo-${movie.Title}" src=${watchlistParsed[movie.Title] ? "./img/removeicon.png" : "./img/addicon.png"} alt="add to watchlist icon" data-add='${movieJson}'>
+                        <p class="no-margin"  id="ar-${movie.Title}" data-add='${movieJson}'>${watchlistParsed[movie.Title] ? "Remove" : "Watchlist"}</p>
+                    </div>
                 </div>
-                <div class="add-to-list">
-                    <img class="no-margin" id="arlogo-${movie.Title}" src="./img/addicon.png" alt="add to watchlist icon" data-add='${movieJson}'>
-                    <p class="no-margin"  id="ar-${movie.Title}" data-add='${movieJson}'>Watchlist</p>
+                <div class="summary no-margin">
+                    <article class="no-margin">
+                        ${checkNA(movie.Plot)}
+                    </article>
                 </div>
-            </div>
-            <div class="summary no-margin">
-                <article class="no-margin">
-                    ${movie.Plot}
-                </article>
             </div>
         </div>
-    </div>
-`)
+    `)
+    }
+    return ''
 }
 
 async function getMovieDetails(title){
     let data = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=d031105a&t=${title}`)
-    let movie = await data.json()
-    return movie
+    let movie = await data.json()               
+    if (!movie.Error) {
+        return movie
+    } else {
+        return false
+    }
 }
 
 
--- MovieWatchList/watchlist.js
+++ MovieWatchList/watchlist.js
@@ -1,37 +1,51 @@
 
+let watchlist = localStorage.getItem("watchlist")
+let watchlistParsed = JSON.parse(watchlist)
+
 function renderWatchlist() {
-    let watchlist = JSON.parse(localStorage.getItem('watchlist'))
     let html = ''
-    for (let movie of Object.values(watchlist.movies)){
-        let movieJson = JSON.stringify(movie).replace(/'/g, "&#039;")
-        html += `    
-        <div class="movie-card">
-        <img class="poster no-margin" src="${movie.Poster}" alt="${movie.Title} poster">
-            <div class="movie-info no-margin">
-                <div class="title no-margin">
-                    <h3 class="no-margin">${movie.Title}</h3>
-                    <div class="rating no-margin">
-                        <i class="fa fa-star no-margin"></i>
-                        <p class="no-margin">${movie.imdbRating}</p>
+    function checkNA(property) {
+        return property === "N/A" ? "" : property
+    }
+    if (Object.keys(watchlistParsed).length > 0) {
+        for (let movie of Object.values(watchlistParsed)){
+            let movieJson = JSON.stringify(movie).replace(/'/g, "&#039;")
+            html += `    
+            <div class="movie-card">
+            <img class="poster no-margin" src="${movie.Poster === "N/A" ? './img/alt-poster.jpg' : movie.Poster}" alt="${checkNA(movie.Title)} poster">
+                <div class="movie-info no-margin">
+                    <div class="title no-margin">
+                        <h3 class="no-margin">${checkNA(movie.Title)}</h3>
+                        <div class="rating no-margin">
+                            <i class="fa fa-star no-margin"></i>
+                            <p class="no-margin">${checkNA(movie.imdbRating)}</p>
+                        </div>
+                    </div>
+                    <div class="details">
+                        <div class="time-genre">
+                            <p class="time">${checkNA(movie.Runtime)}</p> 
+                            <p class="genre">${checkNA(movie.Genre)}</p>
+                        </div>
+                        <div class="add-to-list">
+                            <img class="no-margin" src="./img/removeicon.png"  alt="add to watchlist icon" data-remove='${movieJson}'>
+                            <p class="no-margin" data-remove='${movieJson}'>Remove</p>
+                        </div>
+                    </div>
+                    <div class="summary no-margin">
+                        <article class="no-margin">
+                            ${checkNA(movie.Plot)}
+                        </article>
                     </div>
                 </div>
-                <div class="details">
-                    <div class="time-genre">
-                        <p class="time">${movie.Runtime }</p> 
-                        <p class="genre">${movie.Genre}</p>
-                    </div>
-                    <div class="add-to-list">
-                        <img class="no-margin" src="./img/removeicon.png"  alt="add to watchlist icon" data-remove='${movieJson}'>
-                        <p class="no-margin" data-remove='${movieJson}'>Remove</p>
-                    </div>
-                </div>
-                <div class="summary no-margin">
-                    <article class="no-margin">
-                        ${movie.Plot}
-                    </article>
-                </div>
-            </div>
-        </div>`
+            </div>`
+        }
+    } else {
+        html = `
+        <div class="empty-list">
+            <p>Your watchlist looking a little empty...</p>
+            <a href="./index.html"><img src="./img/addicon.png"> Let's add some movies</a>
+        </div>
+        `
     }
     document.getElementById("watchlist-container").innerHTML = html
 }
@@ -44,9 +58,8 @@ document.addEventListener("click", function(e) {
 })
 
 function removeFromList(movie) {
-    let watchlist = JSON.parse(localStorage.getItem("watchlist"))
-    delete watchlist.movies[movie.Title]
-    localStorage.setItem("watchlist", JSON.stringify(watchlist))
+    delete watchlistParsed[movie.Title]
+    localStorage.setItem("watchlist", JSON.stringify(watchlistParsed))
     renderWatchlist()
 }
 
